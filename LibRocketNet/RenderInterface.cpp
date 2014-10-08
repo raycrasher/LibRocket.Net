f36b4e2fcfcf3368049ecacfab4f19420314a604
zero <cinquedia@gmail.com>
2014-10-09 06:28:35 +0800

    RenderInterface done

--- LibRocketNet/RenderInterface.cpp
+++ LibRocketNet/RenderInterface.cpp
@@ -3,6 +3,7 @@
 #include "LibRocketNet.h"
 #include "RenderInterface.h"
 #include <gcroot.h>
+#include "Util.h"
 
 
 namespace LibRocketNet {
@@ -13,33 +14,130 @@ namespace LibRocketNet {
 
 	class InternalRenderInterface : public Rocket::Core::RenderInterface {
 	public:
+
+		InternalRenderInterface(){
+			
+		}
+
 		gcroot<LibRocketNet::RenderInterface^> Interface;
 
-		virtual void RenderGeometry(RVertex* vertices, int num_vertices, int* indices, int num_indices, RTextureHandle texture, const RVector2f& translation) {
+		
+
+		virtual void RenderGeometry(RVertex* vertices, int num_vertices, int* indices, int num_indices, RTextureHandle texture, const RVector2f& translation)  override {
 			Interface->RenderGeometry((Vertex *)vertices, num_vertices, indices, num_indices, (TextureHandle) (void *)texture, Vector2f(translation.x,translation.y));
 		}
 
-		virtual RCompiledGeometryHandle CompileGeometry(RVertex* vertices, int num_vertices, int* indices, int num_indices, RTextureHandle texture) {
-			return Interface->CompileGeometry((Vertex *)vertices, num_vertices, indices, num_indices, (TextureHandle) (void *)texture).ToInt32;
+		virtual RCompiledGeometryHandle CompileGeometry(RVertex* vertices, int num_vertices, int* indices, int num_indices, RTextureHandle texture) override  {
+			Interface->_methodUnused=false;
+			bool result = Interface->CompileGeometry((Vertex *)vertices, num_vertices, indices, num_indices, (TextureHandle) (void *)texture).ToInt32;
+			if(Interface->_methodUnused)
+				return Rocket::Core::RenderInterface::CompileGeometry(vertices, num_vertices, indices, num_indices, texture);
+			else return result;
 		}
 
-		virtual void RenderCompiledGeometry(RCompiledGeometryHandle geometry, const RVector2f& translation) {
-			return Interface->CompileGeometry((CompiledGeometryHandle)(void *) geometry, new Vector2f(translation));
+		virtual void RenderCompiledGeometry(RCompiledGeometryHandle geometry, const RVector2f& translation)  override {
+			Interface->_methodUnused=false;
+				Interface->RenderCompiledGeometry((CompiledGeometryHandle)(void *) geometry, Vector2f(translation));
+			if(Interface->_methodUnused)
+				Rocket::Core::RenderInterface::RenderCompiledGeometry(geometry, translation);
 		}
-		virtual void ReleaseCompiledGeometry(CompiledGeometryHandle geometry);
-		virtual void EnableScissorRegion(bool enable);
-		virtual void SetScissorRegion(int x, int y, int width, int height);
-		virtual bool LoadTexture(TextureHandle& texture_handle, RVector2i& texture_dimensions, const RocketString& source);
-		virtual bool GenerateTexture(TextureHandle& texture_handle, const unsigned char* source, const RVector2i& source_dimensions);
-		virtual void ReleaseTexture(TextureHandle texture);
-		virtual float GetHorizontalTexelOffset();
-		virtual float GetVerticalTexelOffset();
-		virtual float GetPixelsPerInch();
-		virtual void Release();
+
+		virtual void ReleaseCompiledGeometry(RCompiledGeometryHandle geometry)  override {
+			Interface->_methodUnused=false;
+				Interface->ReleaseCompiledGeometry((CompiledGeometryHandle)(void *) geometry);
+			if(Interface->_methodUnused)
+				Rocket::Core::RenderInterface::ReleaseCompiledGeometry(geometry);
+		}
+
+		virtual void EnableScissorRegion(bool enable)  override {
+			Interface->EnableScissorRegion(enable);
+		}
+
+		virtual void SetScissorRegion(int x, int y, int width, int height)  override {
+			Interface->SetScissorRegion(x,y,width,height);
+		}
+
+		virtual bool LoadTexture(RTextureHandle& texture_handle, RVector2i& texture_dimensions, const RocketString& source)  override {
+			Interface->_methodUnused=false;
+			TextureHandle handle = TextureHandle::Zero;
+			Vector2i dimensions;
+			
+			bool result = Interface->LoadTexture(handle, dimensions, Util::ToNetString(source));
+
+			if(Interface->_methodUnused) return Rocket::Core::RenderInterface::LoadTexture(texture_handle, texture_dimensions, source);
+
+			texture_handle=handle.ToPointer;
+			texture_dimensions.x=dimensions.X;
+			texture_dimensions.y=dimensions.Y;
+
+			return result;
+		}
+
+		virtual bool GenerateTexture(RTextureHandle& texture_handle, const unsigned char* source, const RVector2i& source_dimensions)  override {
+			Interface->_methodUnused=false;
+			TextureHandle handle = TextureHandle::Zero;
+
+			bool result=Interface->GenerateTexture(handle, (System::Byte *) source, Vector2i(source_dimensions));
+			if(Interface->_methodUnused)
+				return Rocket::Core::RenderInterface::GenerateTexture(texture_handle, source, source_dimensions);
+
+			texture_handle = handle.ToPointer;
+			return result;
+		}
+
+
+		virtual void ReleaseTexture(RTextureHandle texture) override {
+			Interface->_methodUnused=false;
+			Interface->ReleaseTexture((TextureHandle)(void*)texture);
+			if(Interface->_methodUnused)
+				Rocket::Core::RenderInterface::ReleaseTexture(texture);
+		}
+
+		virtual float GetHorizontalTexelOffset()  override {
+			Interface->_methodUnused=false;
+			auto result= Interface->GetHorizontalTexelOffset();
+			if(Interface->_methodUnused)
+				return Rocket::Core::RenderInterface::GetHorizontalTexelOffset();
+			else 
+				return result;
+		}
+
+		virtual float GetVerticalTexelOffset()  override {
+			Interface->_methodUnused=false;
+			auto result = Interface->GetVerticalTexelOffset();
+			if(Interface->_methodUnused)
+				return Rocket::Core::RenderInterface::GetVerticalTexelOffset();
+			else 
+				return result;
+		}
+
+		virtual float GetPixelsPerInch()  override {
+			Interface->_methodUnused=false;
+			auto result=Interface->GetPixelsPerInch();
+			if(Interface->_methodUnused)
+				return Rocket::Core::RenderInterface::GetPixelsPerInch();
+			return result;
+		}
+
+		virtual void Release()  override {
+			Interface->_methodUnused=false;
+			Interface->Release();
+			if(Interface->_methodUnused)
+				Rocket::Core::RenderInterface::Release();
+		}
 	};
 
 RenderInterface::RenderInterface(){
+	Interface= (IntPtr) new InternalRenderInterface();
+}
 
+RenderInterface::~RenderInterface(){
+	InternalRenderInterface * ptr = Interface.ToPointer;
+	delete ptr;
+}
+
+Context^ RenderInterface::Context::get(){
+	return _context;
 }
 
 }
--- LibRocketNet/RenderInterface.h
+++ LibRocketNet/RenderInterface.h
@@ -14,8 +14,6 @@ namespace Rocket {
 
 namespace LibRocketNet {
 
-
-
 typedef IntPtr TextureHandle;
 typedef IntPtr CompiledGeometryHandle;
 
@@ -23,27 +21,31 @@ ref class Context;
 
 public ref class RenderInterface abstract
 {
+internal:
+	bool _methodUnused;
 public:
 	RenderInterface();
 	virtual ~RenderInterface();
 
 	virtual void RenderGeometry(Vertex * vertices, int num_vertices, int * indices, int num_indices, IntPtr texture, Vector2f translation) abstract;
-	virtual CompiledGeometryHandle CompileGeometry(Vertex* vertices, int num_vertices, int* indices, int num_indices, TextureHandle texture);
-	virtual void RenderCompiledGeometry(CompiledGeometryHandle geometry, Vector2f translation);
-	virtual void ReleaseCompiledGeometry(CompiledGeometryHandle geometry);
+	virtual CompiledGeometryHandle CompileGeometry(Vertex* vertices, int num_vertices, int* indices, int num_indices, TextureHandle texture) {	_methodUnused=true;	}
+	virtual void RenderCompiledGeometry(CompiledGeometryHandle geometry, Vector2f translation) {	_methodUnused=true;	}
+	virtual void ReleaseCompiledGeometry(CompiledGeometryHandle geometry) {	_methodUnused=true;	}
 	virtual void EnableScissorRegion(bool enable) abstract;
 	virtual void SetScissorRegion(int x, int y, int width, int height) abstract;
-	virtual bool LoadTexture(TextureHandle% texture_handle, Vector2i% texture_dimensions, String^ source);
-	virtual bool GenerateTexture(TextureHandle% texture_handle, Byte* source, Vector2i% source_dimensions);
-	virtual void ReleaseTexture(TextureHandle texture);
-	virtual float GetHorizontalTexelOffset();
-	virtual float GetVerticalTexelOffset();
-	virtual float GetPixelsPerInch();
-	virtual void Release();
+	virtual bool LoadTexture(TextureHandle% texture_handle, Vector2i% texture_dimensions, String^ source) {	_methodUnused=true;	}
+	virtual bool GenerateTexture(TextureHandle% texture_handle, Byte* source, Vector2i source_dimensions) {	_methodUnused=true;	}
+	virtual void ReleaseTexture(TextureHandle texture) {	_methodUnused=true;	}
+	virtual float GetHorizontalTexelOffset() {	_methodUnused=true;	}
+	virtual float GetVerticalTexelOffset() {	_methodUnused=true;	}
+	virtual float GetPixelsPerInch() {	_methodUnused=true;	}
+	virtual void Release() {	_methodUnused=true;	}
+
 	property Context^ Context { LibRocketNet::Context^ get(); }
 
 internal:
 	System::IntPtr Interface;
+	LibRocketNet::Context^ _context;
 };
 
 
