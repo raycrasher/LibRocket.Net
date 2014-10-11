1e3324c093c647e0101b1d664e7903b2a11519e6
Ian Garcia <cinquedia@gmail.com>
2014-10-10 18:11:14 -0700

    SystemInterface implementation

--- LibRocketNet/SystemInterface.cpp
+++ LibRocketNet/SystemInterface.cpp
@@ -2,6 +2,7 @@
 #include <gcroot.h>
 #include "Rocket/Core/SystemInterface.h"
 
+#include "LibRocketNet.h"
 #include "SystemInterface.h"
 #include "Util.h"
 
@@ -17,13 +18,40 @@ namespace LibRocketNet {
 			return Interface->GetElapsedTime();
 		}
 
-		virtual int TranslateString(String& translated, const String& input) override {
+		virtual int TranslateString(RocketString& translated, const RocketString& input) override {
 			Interface->_methodUnused = false;
 			String^ str;
 			int retval = Interface->TranslateString(str, Util::ToNetString(input));
 			if (Interface->_methodUnused) return Rocket::Core::SystemInterface::TranslateString(translated, input);
 			translated = Util::ToRocketString(str);
 			return retval;
+		}
+
+		virtual void JoinPath(RocketString& translated_path, const RocketString& document_path, const RocketString& path) {
+			String^ tPath;
+			Interface->_methodUnused = false;
+			Interface->JoinPath(tPath, Util::ToNetString(document_path), Util::ToNetString(path));
+			if (Interface->_methodUnused) Rocket::Core::SystemInterface::JoinPath(translated_path, document_path,path);
+			else translated_path = Util::ToRocketString(tPath);
+		}
+
+		virtual bool LogMessage(Rocket::Core::Log::Type type, const RocketString& message) {
+			Interface->_methodUnused = false;
+			auto retval = Interface->LogMessage((LogType)type, Util::ToNetString(message));
+			if (Interface->_methodUnused) return Rocket::Core::SystemInterface::LogMessage(type,message);
+			return retval;
+		}
+
+		virtual void ActivateKeyboard() {
+			Interface->_methodUnused = false;
+			Interface->ActivateKeyboard();
+			if (Interface->_methodUnused) Rocket::Core::SystemInterface::ActivateKeyboard();
+		}
+
+		virtual void DeactivateKeyboard() {
+			Interface->_methodUnused = false;
+			Interface->DeactivateKeyboard();
+			if (Interface->_methodUnused) Rocket::Core::SystemInterface::DeactivateKeyboard();
 		}
 
 	};
@@ -33,5 +61,9 @@ namespace LibRocketNet {
 		SystemInterfacePtr = new InternalSystemInterface();
 		SystemInterfacePtr->Interface = this;
 		_methodUnused = false;
+	}
+
+	SystemInterface::~SystemInterface() {
+		delete SystemInterfacePtr;
 	}
 }
